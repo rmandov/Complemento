7e621f0e30bdff36da323511d4f2b329ce89c501
rmandov <josearmandovelascop@gmail.com>
2026-06-18 11:32:56 -0600

    Se crea .vue para el table

---
+++ src/modules/map/views/TableMap.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+<section>
+  <h2>Tabla</h2>
+  <p>Aqui vamos a insertar la carga de la base de datos completa</p>
+</section>
+
+</template>
+
+<style scoped>
+
+section{
+  border: solid 1px orange;
+}
+
+</style>
