0c4b11811b323a2068c73a425f4c7ef89beb8911
santiagogomez06102000 <santiago.gomez06102000@gmail.com>
2026-07-09 12:36:38 -0600

    separacion

---
+++ src/modules/home/components/Hero.vue
@@ -0,0 +1,59 @@
+<script setup>
+import CantidadProyectos from '@/modules/home/components/CantidadProyectos.vue'
+
+</script>
+<template>
+  <section class="hero-section">
+    <div class="hero-animacion">
+      <div class="animacion">Aquí irá una animación</div>
+    </div>
+    <div class="hero-texto">
+      <h1 class="hero-title text-4xl font-bold">
+        Complementariedad y sinergia entre Programas y Proyectos de Inversión (PPI)
+      </h1>
+      <p class="hero-description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusamus dicta vitae
+        fugiat, cupiditate voluptates aliquid quia alias! Doloremque aliquam quidem ad veniam soluta
+        alias vitae culpa facere esse neque?
+      </p>
+      <CantidadProyectos />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+/* Hero */
+.hero-section {
+  display: flex;
+  gap: 24px;
+  margin-bottom: 32px;
+}
+
+.hero-animacion {
+  flex: 2;
+  min-width: 0;
+}
+
+.hero-texto {
+  flex: 3;
+  min-width: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+
+.hero-title {
+  font-family: NotoSansBold;
+}
+
+.animacion {
+  width: 100%;
+  height: 100%;
+  background-color: rgb(242, 242, 242);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import HomeView from '@/modules/map/views/HomeView.vue'
+import HomeView from '@/modules/home/views/HomeView.vue'
 import MapView from '@/modules/map/views/MapView.vue'
 import FichaView from '@/modules/map/views/FichaView.vue'
 
